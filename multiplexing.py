fbc12fdd39cc7cc14e11ad954077b07201f6b774
Sunillad08 <ladsunil08@gmail.com>
2021-05-05 18:50:24 +0530

    added multiplexing file

---
+++ multiplexing.py
@@ -0,0 +1,16 @@
+## Designing multiplexing logic for 
+'''
+1. multiplexer
+2. demultiplexr
+'''
+
+# format to define functions
+
+'''
+define function(type , inputs , selection pins , Enabler)
+type : 2:1 or 8:3 , etc
+inputs : inputs for pins
+selection pins : to select pin
+enable : to activate circuit i.e 0 (optional)
+return output in list/tuple (decide!)
+'''
