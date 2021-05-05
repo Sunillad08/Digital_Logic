7e76ce74ffb57ab3dec4f62e813945a4df934560
Parth Khanolkar <khr0nus144@gmail.com>
2021-05-05 21:35:26 +0530

    Added NOR Gate function

--- Gates.py
+++ Gates.py
@@ -46,6 +46,9 @@ def logic_not(val):
 
 def logic_nand(val1, val2):
   return logic_not(logic_and(val1, val2))
+
+def logic_nor(val1, val2):
+  return logic_not(logic_or(val1, val2))
   
 if __name__ == "__main__":
     print(logic_and(1,0))
