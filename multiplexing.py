b81b2890dd0b760c92ab9b325f36b71af821798b
Sunillad08 <ladsunil08@gmail.com>
2021-05-07 21:30:21 +0530

    added demux and fixed a bug

--- multiplexing.py
+++ multiplexing.py
@@ -16,24 +16,25 @@ return output in list/tuple (decide!)
 '''
 import math
 
-# checking and raising errors
-def check_valid_multiplexer_data(data , level):
-    if level != 2 ** (round(math.sqrt(level))):
-        approx_value = 2 ** (round(math.sqrt(level)))
-        raise ValueError(f"Invalid value : Do you mean {approx_value} ?") # invalid level
-    elif isinstance(data , list) != True:
-        raise SyntaxError("Input should be in list or array") # input not in list
-    elif level != len(data):
-        raise ValueError(f"Insuffiecient data , exact {len(data)+1} required") # insuffient data
-
+# multiplexer with 2,4,8,16,32,64,... levels
 class multiplexer():
 
+    # checking and raising errors
+    def __check_valid_multiplexer_data(data , level):
+        if level != 2**(round(math.log2(level))):
+            approx_value = 2**round(math.log2(level))
+            raise ValueError(f"Invalid value : Do you mean {approx_value} ?") # invalid level
+        elif isinstance(data , list) != True:
+            raise SyntaxError("Input should be in list or array") # input not in list format
+        elif level != len(data):
+            raise ValueError(f"Insuffiecient data , exact {level} required") # insuffient data
+    
     # initializing multiplexer
     def __init__(self , data , level):
-        check_valid_multiplexer_data(data, level)
+        multiplexer.__check_valid_multiplexer_data(data, level)
         self.data = data
         self.level = level
-        self.select_pins_count = round(math.log2(level))
+        self.__select_pins_count = round(math.log2(level))
 
     # returns input based on selector pins status
     def get_input(self , selectpins):
@@ -45,11 +46,11 @@ class multiplexer():
                     raise ValueError("Invalid value for select pins")
             
             # suffiecient pins count checking
-            if len(selectpins) != self.select_pins_count:
-                if len(selectpins) > self.select_pins_count:
-                    raise ValueError(f"Too many values for select pins , only {self.select_pins_count} required")
+            if len(selectpins) != self.__select_pins_count:
+                if len(selectpins) > self.__select_pins_count:
+                    raise ValueError(f"Too many values for select pins , only {self.__select_pins_count} required")
                 else:
-                    raise ValueError(f"Insuffient values for select pins , {self.select_pins_count} required")
+                    raise ValueError(f"Insuffient values for select pins , {self.__select_pins_count} required")
             
             # main logic
             data_pin_number = 0
@@ -78,10 +79,84 @@ class multiplexer():
         
         # updated value
         self.data[data_pin] = new_value
+
+# demultiplexer with 2,4,8,16,32,64,... levels   
+class demultiplexer():
+
+    # checking and raising errors
+    def __check_valid_demultiplexer_data(input_pin , level):
+        if level != 2**(round(math.log2(level))):
+            approx_value = 2**round(math.log2(level))
+            raise ValueError(f"Invalid value : Do you mean {approx_value} ?") # invalid level
+        elif input_pin not in [0,1]:
+            raise SyntaxError("Input should be binary") # input not in binary format
+    
+    # initializing demultiplexer
+    def __init__(self , input_pin , level):
+        demultiplexer.__check_valid_demultiplexer_data(input_pin, level)
+        self.input_pin = input_pin
+        self.level = level
+        self.__select_pins_count = round(math.log2(level))
+        self.output_pins = [-1 for _ in range(level)] # disconnect state for every pin
+        self.__current_pin = 0
+
+    # switch input pin from 0 to 1 and vice verse
+    def switch_input(self):
+        self.input_pin = int(bool(self.input_pin))
+        self.output_pins[self.current_pin] = self.input_pin
+
+    # get current pin on which output is redirected
+    def get_current_pin(self):
+        return self.__current_pin
+    
+    # changes output pin which is connected to input pin
+    def set_select_pin(self , selectpins):
+        # list format compulsion
+        if isinstance(selectpins , list):
+            # binary only
+            for i in selectpins:
+                if i not in [0,1]:
+                    raise ValueError("Invalid value for select pins")
+            
+            # suffiecient pins count checking
+            if len(selectpins) != self.__select_pins_count:
+                if len(selectpins) > self.__select_pins_count:
+                    raise ValueError(f"Too many values for select pins , only {self.__select_pins_count} required")
+                else:
+                    raise ValueError(f"Insuffient values for select pins , {self.__select_pins_count} required")
+            
+            # main logic
+            data_pin_number = 0
+            selectpins.reverse()
+            for i in range(len(selectpins)):
+                data_pin_number += selectpins[i] * (2**i)
+             
+            try:
+                self.output_pins[self.__current_pin] = -1 # deattach output pin
+                self.__current_pin = data_pin_number
+                self.output_pins[data_pin_number] = self.input_pin # connect new pin
+
+            except IndexError:
+                print(f"Error occured , wrong value {data_pin_number}")
+            except :
+                print("Error")
+                print(data_pin_number)
+
+        else:
+            raise SyntaxError("Input should be in list or array")
         
+    # get output pins status
+    def get_output_pin(self):
+        return tuple(self.output_pins) # returns all pins current status
 
 if __name__ == "__main__":
-    m = multiplexer([0,1,0,1,1,1,1,1] , 8)
-    print(m.get_input([0,1,0]))
-    print(m.update_data(2 , 1))
-    print(m.get_input([0,1,0]))
+    # m = multiplexer([0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1] , 128)
+    # print(m.get_input([1,1,1,1,1]))
+    # print(m.update_data(32 , 0))
+    # print(m.get_input([1,1,1,1,1]))
+
+    d = demultiplexer(1,32)
+    print(d.get_current_pin())
+    print(d.get_current_pin())
+    print(d.set_select_pin([1,1,0,1,1]))
+    print(d.get_output_pin())
