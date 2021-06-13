ee63d6b8e2af19d93b9908aa9127f83e857552e0
Parth <khr0nus144@gmail.com>
2021-06-13 16:59:31 +0530

    added shift registers

---
+++ shift_registers.py
@@ -0,0 +1,156 @@
+class shift_register_SIPO():
+    
+    def __init__(self,level,inputno = None):
+        self.level = level
+        self.inputno = inputno
+        
+    
+    def sr_set(self,inputno):
+        
+        #list input 
+        if (isinstance(inputno, list)):
+            if(len(inputno) == self.level):
+                for bin_in in inputno:
+                    if bin_in not in [0,1]:
+                        raise ValueError("Invalid value for input")
+            else:
+                raise ValueError("Number of input bits is not equal to the number of flip flops")
+        else:
+            raise ValueError("Input must be in a list format")
+        self.inputno = inputno
+        
+                
+                
+    def sr_get(self,clock):
+        
+        if(isinstance(clock,int)):
+            if (clock < 0):
+                raise ValueError("Clock pulses are not negative")
+            elif (clock >= self.level):
+                clock = self.level - 1
+                
+        else:
+            raise ValueError("The Nth clock pulse should be an integer")
+        
+        input_cp = self.inputno.copy()
+        og_list = []
+        for i in range(clock + 1):
+            
+            #start from the least significant bit
+            og_list.insert(0,input_cp[-1])
+            input_cp.pop()
+           
+        if(len(og_list)  < self.level):
+            for val in range(self.level - len(og_list)):
+                og_list.append(0)
+                
+
+        return(og_list)  
+        
+            
+            
+
+class shift_register_PISO():
+    
+    def __init__(self,level,inputno = None):
+        self.level = level
+        self.inputno = inputno
+        
+    
+    def sr_set(self,inputno):
+        
+        #list input 
+        if (isinstance(inputno, list)):
+            if(len(inputno) == self.level):
+                for bin_in in inputno:
+                    if bin_in not in [0,1]:
+                        raise ValueError("Invalid value for input")
+            else:
+                raise ValueError("Number of input bits is not equal to the number of flip flops")
+        else:
+            raise ValueError("Input must be in a list format")
+        self.inputno = inputno
+        
+                
+                
+    def sr_get(self,clock):
+        
+        if(isinstance(clock,int)):
+            if (clock < 0):
+                raise ValueError("Clock pulses are not negative")
+            elif (clock >= self.level):
+                clock = self.level - 1
+                
+        else:
+            raise ValueError("The Nth clock pulse should be an integer")
+        
+        input_cp = self.inputno.copy()
+        og_list = []
+        for i in range(clock + 1):
+            
+            #start from the least significant bit
+            og_list.insert(0,input_cp[-1])
+            input_cp.pop()
+           
+        if(len(og_list)  < self.level):
+            for val in range(self.level - len(og_list)):
+                og_list.append(0)
+                
+        
+        return(og_list)  
+        
+class shift_register_SISO():
+    
+    def __init__(self,level,inputno = None):
+        self.level = level
+        self.inputno = inputno
+        
+    
+    def sr_set(self,inputno):
+        
+        #list input 
+        if (isinstance(inputno, list)):
+            if(len(inputno) == self.level):
+                for bin_in in inputno:
+                    if bin_in not in [0,1]:
+                        raise ValueError("Invalid value for input")
+            else:
+                raise ValueError("Number of input bits is not equal to the number of flip flops")
+        else:
+            raise ValueError("Input must be in a list format")
+        self.inputno = inputno
+        
+                
+                
+    def sr_get(self,clock):
+        
+        if(isinstance(clock,int)):
+            if (clock < 0):
+                raise ValueError("Clock pulses are not negative")
+            elif (clock >= self.level):
+                clock = self.level - 1
+                
+        else:
+            raise ValueError("The Nth clock pulse should be an integer")
+        
+        input_cp = self.inputno.copy()
+        onlist = []
+        for i in range(clock+1):
+            onlist.append([0] * (clock+1))
+        og_list = []
+        for i in range(clock + 1):
+            
+            #start from the least significant bit
+            og_list.insert(0,input_cp[-1])
+            input_cp.pop()
+            if(len(og_list)  < self.level):
+                for val in range(self.level - len(og_list)):
+                    og_list.append(0)
+            print(og_list)
+            #onlist.append(og_list)   
+        
+        #return(onlist)             
+
+sr1 = shift_register_SISO(4)
+sr1.sr_set([1,0,1,1])
+print(sr1.sr_get(8))
